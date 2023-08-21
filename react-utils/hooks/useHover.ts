36feed6595ae54aa5b3da763efc125956252171b
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-08-21 23:42:27 +0200

    feat(hooks): useHover

---
+++ react-utils/hooks/useHover.ts
@@ -0,0 +1,28 @@
+import { ForwardedRef, MutableRefObject, useEffect, useRef, useState } from "react"
+
+export const useHover = <T extends HTMLElement | null>(): [
+  ForwardedRef<T> | MutableRefObject<T>, boolean,
+] => {
+  const [value, setValue] = useState(false);
+  const ref = useRef<T>(null);
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  useEffect(
+    () => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener('mouseover', handleMouseOver);
+      node.addEventListener('mouseout', handleMouseOut);
+      return () => {
+        node.removeEventListener('mouseover', handleMouseOver);
+        node.removeEventListener('mouseout', handleMouseOut);
+      }
+    }
+    return null;
+  },
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [ref.current] // Recall only if ref changes
+  );
+  return [ref, value];
+}

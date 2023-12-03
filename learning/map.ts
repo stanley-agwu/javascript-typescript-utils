fc157033f19ce732ac1d04ddf859a67a834c63c1
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-12-03 06:57:29 +0100

    feat(Learning): Map - updated

--- learning/map.ts
+++ learning/map.ts
@@ -19,3 +19,17 @@ console.log('has main =>', usersMap.has('main')); // returns true or false if it
 usersMap.forEach(user => console.log('user', user)); // forEach returns values of of each item
 usersMap.clear(); // clears all items from map, returns an empty map
 console.log(usersMap);
+console.log(Array.from(usersMap));
+
+// Method	Description
+// new Map()	Creates a new Map object
+// set()	Sets the value for a key in a Map
+// get()	Gets the value for a key in a Map
+// clear()	Removes all the elements from a Map
+// delete()	Removes a Map element specified by a key
+// has()	Returns true if a key exists in a Map
+// forEach()	Invokes a callback for each key/value pair in a Map
+// entries()	Returns an iterator object with the [key, value] pairs in a Map
+// keys()	Returns an iterator object with the keys in a Map
+// values()	Returns an iterator object of the values in a Map
+// size	Returns the number of Map elements
